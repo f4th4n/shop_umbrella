cdf90b2383a774ac2887eff04b703f2e77190ac1
fathan <wilfathan@gmail.com>
2022-06-18 15:15:05 +0700

    Finish multi game room

--- apps/shop_web/lib/shop_web/channels/mini_game/game_channel.ex
+++ apps/shop_web/lib/shop_web/channels/mini_game/game_channel.ex
@@ -3,24 +3,32 @@ defmodule ShopWeb.MiniGame.GameChannel do
   import ShopWeb.Gettext
 
   def join("game:" <> room, payload, socket) do
+    Shop.Game.Supervisor.start_child(room)
+
     {:ok, socket}
   end
 
   def handle_in("game:start", payload, socket) do
+    game_state = get_room(socket.topic) |> Shop.Game.GenServer.state()
+
     res = %{
       game_title: gettext("reduce calculatorz"),
-      game_state: Shop.Game.GenServer.state()
+      game_state: game_state
     }
 
     {:reply, {:ok, res}, socket}
   end
 
   def handle_in("game:hit", payload, socket) do
+    game_state = get_room(socket.topic) |> Shop.Game.GenServer.reduce()
+
     res = %{
-      game_state: Shop.Game.GenServer.reduce()
+      game_state: game_state
     }
 
     broadcast!(socket, "game:new_state", res)
     {:noreply, socket}
   end
+
+  defp get_room("game:" <> room), do: room
 end
